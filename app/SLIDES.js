e55baa98c2c3e941fd53f35ab4f68d93f3f6aa61
Jordan Papaleo <papaleowebdev@gmail.com>
2016-03-18 10:15:48 -0700

    ES6 string templates implemented

--- app/SLIDES.js
+++ app/SLIDES.js
@@ -14,27 +14,21 @@
 
 export default [
   {
-    id: 'overview',
-    className: 'step',
-    attributes: {
-      x: 3000,
-      y: 1500,
-      scale: 10
-    }
-  },
-  {
     id: 'start',
     className: 'step slide',
     attributes: {
       x: -1500,
       y: -3000
     },
-    content: `
-      <div class='inner-slide flex-centered flex-column'>
-        <h1>Is this right for you</h1>
-        <div><p>presented by</p></div>
-      </div>
-      <img style='position: absolute; bottom: 10px; right: 10px;' src='${require('assets/images/ct_header.svg')}' />`
+    get content () {
+      return `
+        <div class='inner-slide flex-centered flex-column'>
+          <h1>Is this right for you</h1>
+          <div><p>presented by</p></div>
+        </div>
+        <img style='position: absolute; bottom: 10px; right: 10px;' src='${require('assets/images/ct_header.svg')}' />
+      `
+    }
   },
   {
     id: 'step-1',
@@ -42,7 +36,11 @@ export default [
     attributes: {
       y: -3000
     },
-    content: '<q>Don’t you think that presentations given <strong>in modern browsers</strong> shouldn’t <strong>copy the limits</strong> of ‘classic’ slide decks?</q>'
+    get content () {
+      return `
+        <q>Don’t you think that presentations given <strong>in modern browsers</strong> shouldn’t <strong>copy the limits</strong> of ‘classic’ slide decks?</q>
+      `
+    }
   },
   {
     id: 'step-2',
@@ -53,7 +51,11 @@ export default [
       z: -6000,
       rotate: 180
     },
-    content: '<q>Would you like to <strong>impress your audience</strong> with <strong>stunning visualization</strong> of your talk?</q>'
+    get content () {
+      return `
+        <q>Would you like to <strong>impress your audience</strong> with <strong>stunning visualization</strong> of your talk?</q>
+      `
+      }
   },
   {
     id: 'title',
@@ -65,11 +67,13 @@ export default [
       scale: 4,
       rotate: 0
     },
-    content: `
-      <span class='try'>then you should try</span>
-      <h1>impress.js<sup>*</sup></h1>
-      <span class='footnote'><sup>*</sup> no rhyme intended</span>
-    `
+    get content () {
+      return `
+        <span class='try'>then you should try</span>
+        <h1>impress.js<sup>*</sup></h1>
+        <span class='footnote'><sup>*</sup> no rhyme intended</span>
+      `
+    }
   },
   {
     id: 'its',
@@ -81,7 +85,11 @@ export default [
       scale: 5,
       rotate: 90
     },
-    content: '<p>It’s a <strong>presentation tool</strong> <br/> inspired by the idea behind <a href=\'http://prezi.com\'>prezi.com</a> <br/> and based on the <strong>power of CSS3 transforms and transitions</strong> in modern browsers.</p>'
+    get content () {
+      return `
+        <p>It is a <strong>presentation tool</strong> <br/> inspired by the idea behind <a href='http://prezi.com'>prezi.com</a> <br/> and based on the <strong>power of CSS3 transforms and transitions</strong> in modern browsers.</p>
+      `
+    }
   },
   {
     id: 'big',
@@ -93,7 +101,11 @@ export default [
       scale: 6,
       rotate: 180
     },
-    content: '<p>visualize your <b>big</b> <span class=\'thoughts\'>thoughts</span></p>'
+    get content () {
+      return `
+        <p>visualize your <b>big</b> <span class='thoughts'>thoughts</span></p>
+      `
+    }
   },
   {
     id: 'tiny',
@@ -105,7 +117,11 @@ export default [
       scale: 1,
       rotate: 300
     },
-    content: '<p>and <b>tiny</b> ideas</p>'
+    get content () {
+      return `
+        <p>and <b>tiny</b> ideas</p>
+      `
+    }
   },
   {
     id: 'ing',
@@ -117,7 +133,11 @@ export default [
       scale: 6,
       rotate: 0
     },
-    content: '<p>by <b class=\'positioning\'>positioning</b>, <b class=\'rotating\'>rotating</b> and <b class=\'scaling\'>scaling</b> them on an infinite canvas</p>'
+    get content () {
+      return `
+        <p>by <b class='positioning'>positioning</b>, <b class='rotating'>rotating</b> and <b class='scaling'>scaling</b> them on an infinite canvas</p>
+      `
+    }
   },
   {
     id: 'imagination',
@@ -129,7 +149,11 @@ export default [
       scale: 8,
       rotate: -60
     },
-    content: '<p>the only <b>limit</b> is your <b class=\'imagination\'>imagination</b></p>'
+    get content () {
+      return `
+        <p>the only <b>limit</b> is your <b class='imagination'>imagination</b></p>
+      `
+    }
   },
   {
     id: 'source',
@@ -141,10 +165,12 @@ export default [
       scale: 4,
       rotate: 20
     },
-    content: `
-      p>want to know more?</p>
-      <q><a href='http://github.com/bartaz/impress.js'>use the source</a>, Luke!</q>
-    `
+    get content () {
+      return `
+        <p>want to know more?</p>
+        <q><a href='http://github.com/bartaz/impress.js'>use the source</a>, Luke!</q>
+      `
+    }
   },
   {
     id: 'one-more-thing',
@@ -167,17 +193,19 @@ export default [
       'rotate-x': -40,
       'rotate-y': 10
     },
-    content: `
-      <p>
-        <span class='have'>have</span>
-        <span class='you'>you</span>
-        <span class='noticed'>noticed</span>
-        <span class='its'>it’s</span>
-        <span class='in'>in</span>
-        <b>3D<sup>*</sup></b>?
-      </p>
-      <span class='footnote'>* beat that, prezi ;)</span>
-    `
+    get content () {
+      return `
+        <p>
+          <span class='have'>have</span>
+          <span class='you'>you</span>
+          <span class='noticed'>noticed</span>
+          <span class='its'>it’s</span>
+          <span class='in'>in</span>
+          <b>3D<sup>*</sup></b>?
+        </p>
+        <span class='footnote'>* beat that, prezi ;)</span>
+      `
+    }
   },
   {
     id: 'credits',
@@ -191,16 +219,18 @@ export default [
       'rotate-x': -40,
       'rotate-y': 10
     },
-    content: `
-    <div class='inner-slide flex-centered flex-column'>
-      <h1>Built using</h1>
-      <ul>
-        <li>HTML</li>
-        <li>CSS/SCSS</li>
-        <li>JavaScript</li>
-        <li>ImpressJS</li>
-      </ul>
-    </div>
-    `
+    get content () {
+      return `
+        <div class='inner-slide flex-centered flex-column'>
+          <h1>Built using</h1>
+          <ul>
+            <li>HTML</li>
+            <li>CSS/SCSS</li>
+            <li>JavaScript</li>
+            <li>ImpressJS</li>
+          </ul>
+        </div>
+      `
+    }
   }
 ]
